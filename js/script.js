1889b766229e6eb8ef1e700dff75553022f702df
micheleTummolo <tummolomichele02@gmail.com>
2022-12-07 17:24:51 +0100

    Added bombs script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const playButton = document.getElementById('play_button')
 
 playButton.addEventListener ('click', function(){
+
     let diff = document.getElementById('difficulty').value
     console.log("Difficoltà: " + diff)
     let max = 0;
     let grid = document.getElementById('grid');
     let squareType
+
+    grid.innerHTML = ""
     
     if (diff == "easy" ){
         max = 100
@@ -23,12 +26,21 @@ playButton.addEventListener ('click', function(){
 
     console.log("Numero quadrati: " + max)
 
+    let bombs = arrayBomb(1, max)
+    console.log(bombs)
+
     for (let i = 0; i < max; i++) {
         const square = createGridSquare()
         grid.appendChild(square)
         square.addEventListener ('click', function(){
-            this.classList.add ('clicked')
-            console.log("Hai cliccato il quadratino numero " + (i + 1))
+            if (bombs.includes(i+1)) {
+                this.classList.add ('red')
+                console.log("Hai cliccato una bomba")
+            }
+            else {
+                this.classList.add ('clicked')
+                console.log("Hai cliccato il quadratino numero " + (i + 1))
+            }
         })
     };
 
@@ -39,6 +51,22 @@ playButton.addEventListener ('click', function(){
     
         return element;
     };
+
+    function arrayBomb(min, max) {
+        let bombs = []
+        let i = 0
+        
+        while (i < 16) {
+            let bombNum = Math.floor(Math.random() * (max - min + 1) + min)
+
+            if (!bombs.includes(bombNum)){
+                bombs.push(bombNum)
+                i++
+            }
+        } 
+
+        return bombs
+    }
 })
 
 
