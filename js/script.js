935078b9293885fc750164054abbd0ecc4a23708
micheleTummolo <tummolomichele02@gmail.com>
2023-05-05 13:12:13 +0200

    Added h2 tag and id

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const playButton = document.getElementById('play_button')
 let best = 0
 let currentScore
 
-playButton.addEventListener ('click', function(){
+playButton.addEventListener('click', function () {
 
     let diff = document.getElementById('difficulty').value
     console.log("Difficoltà: " + diff)
@@ -12,13 +12,13 @@ playButton.addEventListener ('click', function(){
     let squareType
 
     grid.innerHTML = ""
-    
-    if (diff == "easy" ){
+
+    if (diff == "easy") {
         max = 100
         squareType = 'square_easy'
     }
 
-    else if (diff == "medium"){
+    else if (diff == "medium") {
         max = 81
         squareType = 'square_medium'
     }
@@ -32,7 +32,7 @@ playButton.addEventListener ('click', function(){
     let bombs = arrayBomb(1, max)
     console.log(bombs)
 
-    let gameOver = document.getElementById ('game_over_container')
+    let gameOver = document.getElementById('game_over_container')
     let win = document.getElementById('win_container')
 
     gameOver.classList.remove('active')
@@ -45,15 +45,18 @@ playButton.addEventListener ('click', function(){
 
     let score = document.getElementById('score')
     let bestScore = document.getElementById('best_score')
-    let counter = 0 
+    let counter = 0
     score.innerHTML = ("Score: " + counter)
-    
+
     for (let i = 0; i < max; i++) {
         const square = createGridSquare()
         grid.appendChild(square)
-        square.addEventListener ('click', function(){
-            if (bombs.includes(i+1)) {
-                this.classList.add ('red')
+        let h2 = document.getElementsByTagName('h2')
+        h2[i].setAttribute("id", i + 1)
+        console.log(h2[i].id)
+        square.addEventListener('click', function () {
+            if (bombs.includes(i + 1)) {
+                this.classList.add('red')
                 grid.classList.add('event-none')
                 console.log("Hai cliccato una bomba")
                 gameOver.classList.remove('inactive')
@@ -65,7 +68,7 @@ playButton.addEventListener ('click', function(){
                 }
             }
             else {
-                this.classList.add ('clicked')
+                this.classList.add('clicked')
                 console.log("Hai cliccato il quadratino numero " + (i + 1))
                 counter += 1
                 score.innerHTML = ("Score: " + counter)
@@ -82,30 +85,33 @@ playButton.addEventListener ('click', function(){
                 }
             }
         })
-        
+
     };
 
 
     function createGridSquare() {
-        const element = document.createElement ('div');
-    
-        element.classList.add ('square', squareType);
-    
+        const element = document.createElement('div');
+        const content = document.createElement('h2');
+
+        element.appendChild(content);
+
+        element.classList.add('square', squareType);
+
         return element;
     };
 
     function arrayBomb(min, max) {
         let bombs = []
         let i = 0
-        
+
         while (i < 16) {
             let bombNum = Math.floor(Math.random() * (max - min + 1) + min)
 
-            if (!bombs.includes(bombNum)){
+            if (!bombs.includes(bombNum)) {
                 bombs.push(bombNum)
                 i++
             }
-        } 
+        }
 
         return bombs
     }
